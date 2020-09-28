16fd6b3ea6b8bf01ead274464976f2b45facd546
Parvez M Robin <this@parvezmrobin.com>
2020-09-28 20:59:44 +0600

    groups

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,16 +5,11 @@
   <button @click="append">Append</button>
   <div style="display: flex; justify-content: center;">
     <table>
-      <thead>
-        <tr>
-          <th>A</th>
-          <th>B</th>
-        </tr>
-      </thead>
       <tbody>
-        <tr v-for="{a, b} in list">
-          <td>{{a}}</td>
-          <td>{{b}}</td>
+        <tr v-for="group in groups">
+          <td>
+            {{ [...group.keys()].join(', ') }}
+          </td>
         </tr>
       </tbody>
     </table>
@@ -22,7 +17,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onBeforeUnmount } from "vue";
+import { defineComponent, ref, onMounted, onBeforeUnmount, computed } from "vue";
 import Database from "../Database";
 import { Pair } from "../schema";
 
@@ -40,11 +35,44 @@ export default defineComponent({
     const a = ref('');
     const b = ref('');
     const list = ref<Pair[]>([]);
+
+    const groups = computed(() => {
+      const groups = [] as Map<string, number>[];
+      for (const pair of list.value) {
+        let handled = false;
+        for (const group of groups) {
+          if (group.has(pair.a)) {
+            if (!group.has(pair.b)) {
+              group.set(pair.b, 1);
+            } else {
+              group.set(pair.b, group.get(pair.b) + 1);
+            }
+            group.set(pair.a, group.get(pair.a) + 1);
+            handled = true;
+            break;
+          } else if (group.has(pair.b)) {
+            group.set(pair.a, 1);
+            group.set(pair.b, group.get(pair.b) + 1);
+            handled = true;
+            break;
+          }
+        }
+
+        if (!handled) {
+          const newGroup = new Map<string, number>().set(pair.a, 1).set(pair.b, 1);
+          groups.push(newGroup);
+        }
+      }
+
+      return groups;
+    });
+
     const append = async () => {
       const pair = { a: a.value, b: b.value };
       list.value.push(pair);
       await db.insertOne(pair);
     };
+
     onMounted(async () => {
       db = (await Database.get());
       const pairs = await db.find({}).asArray();
@@ -56,6 +84,7 @@ export default defineComponent({
       a,
       b,
       list,
+      groups,
       append,
     }
   },
