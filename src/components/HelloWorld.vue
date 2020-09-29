fa45d5ec14dc730397e249bbd339911e1f403b69
Parvez M Robin <this@parvezmrobin.com>
2020-09-29 19:06:47 +0600

    username support

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,21 @@
       <div class="col-md-8 offset-md-2">
         <h1 class="text-center">{{ header }}</h1>
         <hr>
-        <form @submit.prevent="append">
+        <form
+          v-if="!isUsernameSet"
+          @submit.prevent="setUsername"
+        >
+          <div class="input-group mb-3">
+            <input type="text" v-model="username" class="form-control" placeholder="Username" title="Username">
+            <div class="input-group-append">
+              <button type="submit" class="btn btn-outline-success">Set Username</button>
+            </div>
+          </div>
+        </form>
+        <form
+          v-else
+          @submit.prevent="append"
+        >
           <div class="input-group mb-3">
             <input type="text" v-model="a" class="form-control" placeholder="First Word" title="First Word">
             <input type="text" v-model="b" class="form-control" placeholder="Second Word" title="First Word">
@@ -31,11 +45,116 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onBeforeUnmount, computed } from "vue";
+import { defineComponent, ref, watch, onBeforeUnmount, computed, Ref } from "vue";
 import Database from "../Database";
 import { Pair } from "../schema";
 
 let db;
+
+function useUsername(isUsernameSet: Ref<boolean>) {
+  const username = ref('');
+
+  function setUsername() {
+    isUsernameSet.value = true;
+  }
+
+  return {
+    username,
+    setUsername,
+  };
+}
+
+function useSynonym(isUsernameSet: Ref<boolean>, username: Ref<string>) {
+  const a = ref('');
+  const b = ref('');
+  const list = ref<Pair[]>([]);
+
+  watch(isUsernameSet, async () => {
+    db = (await Database.get());
+    const pairs = await db.find({ username: username.value }).asArray();
+    list.value = pairs;
+  });
+  onBeforeUnmount(Database.clear);
+
+  const append = async () => {
+    const pair = { username: username.value, a: a.value, b: b.value };
+    list.value.push(pair);
+    await db.insertOne(pair);
+  };
+
+  const groups = computed(() => {
+    const groups = [] as Map<string, number>[];
+
+    function merge(i, j, pair: Pair) {
+      const groupJ = groups[j];
+      groups.splice(j, 1);
+
+      for (const key of Array.from(groupJ.keys())) {
+        groups[i].set(key, groupJ.get(key));
+      }
+      groups[i].set(pair.a, groups[i].get(pair.a) + 1);
+      groups[i].set(pair.b, groups[i].get(pair.b) + 1);
+    }
+
+    for (const pair of list.value) {
+      let handled = false;
+      let aFoundAt = -1;
+      let bFoundAt = -1;
+      for (let i = 0; i < groups.length; i++) {
+        const group = groups[i];
+        if (group.has(pair.a)) {
+          if (bFoundAt !== -1) {
+            merge(bFoundAt, i, pair);
+            break;
+          }
+          aFoundAt = i;
+          if (!group.has(pair.b)) {
+            group.set(pair.b, 1);
+          } else {
+            bFoundAt = i;
+            group.set(pair.b, group.get(pair.b) + 1);
+          }
+          group.set(pair.a, group.get(pair.a) + 1);
+          handled = true;
+        } else if (group.has(pair.b)) {
+          if (aFoundAt !== -1) {
+            merge(aFoundAt, i, pair);
+            break;
+          }
+          bFoundAt = i;
+          group.set(pair.a, 1);
+          group.set(pair.b, group.get(pair.b) + 1);
+          handled = true;
+        }
+
+        if (aFoundAt !== -1 && bFoundAt !== -1) {
+          break;
+        }
+      }
+
+      if (!handled) {
+        const newGroup = new Map<string, number>().set(pair.a, 1).set(pair.b, 1);
+        groups.push(newGroup);
+      }
+    }
+
+    const sortedGroups = groups.map(
+      (group) => Array.from(group.entries())
+        .sort(([, c1], [, c2]) => c2 - c1)
+        .map(([word]) => word),
+    );
+
+    return sortedGroups;
+  });
+
+  return {
+    a,
+    b,
+    list,
+    groups,
+    append,
+  }
+}
 
 export default defineComponent({
   name: 'HelloWorld',
@@ -46,94 +165,14 @@ export default defineComponent({
     }
   },
   setup() {
-    const a = ref('');
-    const b = ref('');
-    const list = ref<Pair[]>([]);
-
-    const groups = computed(() => {
-      const groups = [] as Map<string, number>[];
-
-      function merge(i, j, pair: Pair) {
-        const groupJ = groups[j];
-        groups.splice(j, 1);
-
-        for (const key of Array.from(groupJ.keys())) {
-          groups[i].set(key, groupJ.get(key));
-        }
-        groups[i].set(pair.a, groups[i].get(pair.a) + 1);
-        groups[i].set(pair.b, groups[i].get(pair.b) + 1);
-      }
-
-      for (const pair of list.value) {
-        let handled = false;
-        let aFoundAt = -1;
-        let bFoundAt = -1;
-        for (let i = 0; i < groups.length; i++){
-          const group = groups[i];
-          if (group.has(pair.a)) {
-            if (bFoundAt !== -1) {
-              merge(bFoundAt, i, pair);
-              break;
-            }
-            aFoundAt = i;
-            if (!group.has(pair.b)) {
-              group.set(pair.b, 1);
-            } else {
-              bFoundAt = i;
-              group.set(pair.b, group.get(pair.b) + 1);
-            }
-            group.set(pair.a, group.get(pair.a) + 1);
-            handled = true;
-          } else if (group.has(pair.b)) {
-            if (aFoundAt !== -1) {
-              merge(aFoundAt, i, pair);
-              break;
-            }
-            bFoundAt = i;
-            group.set(pair.a, 1);
-            group.set(pair.b, group.get(pair.b) + 1);
-            handled = true;
-          }
-
-          if (aFoundAt !== -1 && bFoundAt !== -1) {
-            break;
-          }
-        }
-
-        if (!handled) {
-          const newGroup = new Map<string, number>().set(pair.a, 1).set(pair.b, 1);
-          groups.push(newGroup);
-        }
-      }
-
-      const sortedGroups = groups.map(
-        (group) => Array.from(group.entries())
-          .sort(([, c1], [, c2]) => c2 - c1)
-          .map(([word]) => word),
-      );
-
-      return sortedGroups;
-    });
-
-    const append = async () => {
-      const pair = { a: a.value, b: b.value };
-      list.value.push(pair);
-      await db.insertOne(pair);
-    };
-
-    onMounted(async () => {
-      db = (await Database.get());
-      const pairs = await db.find({}).asArray();
-      list.value = pairs;
-    });
-    onBeforeUnmount(Database.clear);
+    const isUsernameSet = ref(false);
+    const { username, setUsername } = useUsername(isUsernameSet);
 
     return {
-      a,
-      b,
-      list,
-      groups,
-      append,
+      isUsernameSet,
+      username,
+      setUsername,
+      ...useSynonym(isUsernameSet, username),
     }
   },
 });
